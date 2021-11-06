f1a8fe4634c985df62faab861b9043c8ab6ca1d0
EslamSallam <eslam.salam1369@gmail.com>
2021-11-06 15:18:40 +0200

    Adding Name Field in Registration

--- GigHub/Models/AccountViewModels.cs
+++ GigHub/Models/AccountViewModels.cs
@@ -68,6 +68,11 @@ namespace GigHub.Models
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
+        
+        [Required]
+        [MaxLength(100)]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
