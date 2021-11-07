4e807cd172043549eeb45343d97b290ec7516c0b
EslamSallam <eslam.salam1369@gmail.com>
2021-11-07 19:18:24 +0200

    Adding Attendances API

---
+++ GigHub/Controllers/AttendacesController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using GigHub.Models;
+using Microsoft.AspNet.Identity;
+
+namespace GigHub.Controllers
+{
+    public class AttendacesController : ApiController
+    {
+        private ApplicationDbContext _context;
+        public AttendacesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [Authorize]
+        [HttpPost]
+        public IHttpActionResult AddAttendance([FromBody] int GigId_)
+        {
+            var attendance_ = new Attendance 
+            { 
+                GigId = GigId_,
+                AttendeeId = User.Identity.GetUserId()
+            };
+            _context.Attendances.Add(attendance_);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+    }
+}
