6f57b29690599f1a324722695cabdf89077a7d1c
EslamSallam <eslam.salam1369@gmail.com>
2021-11-11 16:58:09 +0200

    Implementing the Ajax Request

--- GigHub/Controllers/AttendancesController.cs
+++ GigHub/Controllers/AttendancesController.cs
@@ -5,31 +5,36 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using GigHub.Models;
+using GigHub.ViewModels;
 using Microsoft.AspNet.Identity;
 
 namespace GigHub.Controllers
 {
-    public class AttendacesController : ApiController
+    public class AttendancesController : ApiController
     {
         private ApplicationDbContext _context;
-        public AttendacesController()
+        public AttendancesController()
         {
             _context = new ApplicationDbContext();
         }
 
-        [Authorize]
         [HttpPost]
-        public IHttpActionResult AddAttendance([FromBody] int GigId_)
+        public IHttpActionResult Attend([FromUri] int GiId)
         {
+            string UserId = "cd173de5-0724-4800-b784-ac326f1db37c";
+            if (_context.Attendances.Any(g => g.GigId == GiId && g.AttendeeId == UserId))
+            {
+                return BadRequest("The attendance already exists");
+            }
             var attendance_ = new Attendance 
             { 
-                GigId = GigId_,
-                AttendeeId = User.Identity.GetUserId()
+                GigId = GiId,
+                AttendeeId = UserId
             };
             _context.Attendances.Add(attendance_);
             _context.SaveChanges();
 
-            return Ok();
+            return Ok("Success");
         }
 
     }
